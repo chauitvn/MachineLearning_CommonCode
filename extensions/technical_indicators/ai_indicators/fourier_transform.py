5e7def4aba60fe53b3d29b4fb253b55954a53749
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-07-04 00:59:43 +0800

    add fourier_transform

---
+++ extensions/technical_indicators/ai_indicators/fourier_transform.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from scipy.fft import rfft, rfftfreq, irfft
+from extensions.technical_indicators.indicator_base import Indicator_Base
+
+
+class FourierTransform(Indicator_Base):
+    def __init__(self, data) -> None:
+        self.DATA = data
+        super().__init__()
+    
+    def calculate(self):
+        orginal_df = self.DATA[[self.adj_close]]
+        trend_df = rfft(orginal_df[self.adj_close].values)
+        trend_df[39:-39] = 0
+        merge_df = pd.DataFrame(irfft(trend_df), columns=["Fourier"])
+
+        # merger Fourier data into parent data
+        orginal_df.reset_index(drop=False, inplace=True)
+        orginal_df = pd.concat([orginal_df, merge_df], axis=1)
+        # to make sure the index of two data frame is the same type.
+        orginal_df.set_index("date", inplace=True)
+
+        self.DATA["Fourier"] = orginal_df["Fourier"]
+        return self.DATA
