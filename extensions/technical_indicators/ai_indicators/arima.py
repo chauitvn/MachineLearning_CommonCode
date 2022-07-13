c705cade1aa5859beaadb66e81a9792033a877db
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-07-14 00:55:40 +0800

    add more AI indicator

---
+++ extensions/technical_indicators/ai_indicators/arima.py
@@ -0,0 +1,16 @@
+from statsmodels.tsa.arima_model import ARIMA
+from extensions.technical_indicators.indicator_base import Indicator_Base
+
+class ArimaIndicator(Indicator_Base):
+    def __init__(self, data) -> None:
+        self.DATA = data
+        super().__init__()
+
+    def calculate(self):
+        series = self.data[self.adj_close].diff(periods=1)
+
+        model = ARIMA(series, order=(5, 1, 0))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast()
+        self.DATA['ARIMA'] = output[0]
+        return self.DATA
--- extensions/technical_indicators/technical_indicators.py
+++ extensions/technical_indicators/technical_indicators.py
@@ -1,3 +1,5 @@
+from extensions.technical_indicators.ai_indicators.arima import ArimaIndicator
+from extensions.technical_indicators.ai_indicators.fourier_transform import FourierTransform
 from extensions.technical_indicators.ai_indicators.neural_prophet import NeuralFbProphet
 from extensions.technical_indicators.commons.common_indicators import CommonIndicator
 from extensions.technical_indicators.indicator_base import Indicator_Base
@@ -16,3 +18,9 @@ class TechnicalIndicators(Indicator_Base):
         # FB Prophet indicator
         neural_prophet = NeuralFbProphet(self.DATA)
         self.DATA = neural_prophet.calculate()
+        # fourier transform indicator
+        fourier = FourierTransform(self.DATA)
+        self.DATA = fourier.calculate()
+        # ARiMA Indicator
+        arima = ArimaIndicator(self.DATA)
+        self.DATA = arima.calculate()
