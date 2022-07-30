3183affd4da266563d23bb14ed93541aa9026168
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-07-30 21:18:07 +0800

    fix bug arima

--- extensions/technical_indicators/ai_indicators/arima.py
+++ extensions/technical_indicators/ai_indicators/arima.py
@@ -1,4 +1,4 @@
-from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 from extensions.technical_indicators.indicator_base import Indicator_Base
 
 class ArimaIndicator(Indicator_Base):
@@ -7,10 +7,9 @@ class ArimaIndicator(Indicator_Base):
         super().__init__()
 
     def calculate(self):
-        series = self.data[self.close].diff(periods=1)
-
+        series = self.DATA[self.close].diff(periods=1)
         model = ARIMA(series, order=(5, 1, 0))
-        model_fit = model.fit(disp=0)
-        output = model_fit.forecast()
-        self.DATA['ARIMA'] = output[0]
+        model_fit = model.fit()
+        print(model_fit.summary())
+        self.DATA['ARIMA'] = model_fit.forecast()
         return self.DATA
