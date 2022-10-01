da1773454014390aef0a67823ce17478cb63125e
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-10-01 20:56:28 +0800

    add configuration list

--- StockPredictionPortal/StockPredictionApp/views.py
+++ StockPredictionPortal/StockPredictionApp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Configuration
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, "default.html", {"title_of_page": "Index Page",
                                             "number_off_stock": 1})
+
+def configuration_index(request):
+    context = {
+        "title_of_page": "Configuration"
+    }
+    return render(request, "configuration.html", context)
+
+def configuration(request):
+    config_list = Configuration.objects.all()
+
+    json=[]
+    for item in config_list:
+        json.append({'key':item.key, 'value':item.value, 'HasActive':item.HasActive})
+
+    result = {
+        "title_of_page": "Configuration",
+        "data": config_list
+    }
+
+    return JsonResponse(config_list)
--- StockPredictionPortal/StockPredictionPortal/urls.py
+++ StockPredictionPortal/StockPredictionPortal/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("p/", include("StockPredictionApp.urls")),
+    path("app/", include("StockPredictionApp.urls")),
     path("g/", include("GatheringDataApp.urls")),
 ]
