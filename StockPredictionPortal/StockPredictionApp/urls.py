8a818e9b11be54c56a44aacf7c06eb06085b57da
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-10-02 01:54:07 +0800

    Update Configuration

--- StockPredictionPortal/StockPredictionApp/urls.py
+++ StockPredictionPortal/StockPredictionApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('c/index', views.configuration_index),
+    path('c/index', views.configuration_index, name='c_index'),
     path('c/lst', views.configuration, name ="allConfiguration")
 ]
