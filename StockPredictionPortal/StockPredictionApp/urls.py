b1c815916e5f3323a88804d61c350f61ffdc1512
Pham Dinh Hau <phamdinhhau@gmail.com>
2022-10-09 00:11:03 +0700

    modified:   StockPredictionPortal/StockPredictionApp/templates/configuration.html 	modified:   StockPredictionPortal/StockPredictionApp/urls.py

--- StockPredictionPortal/StockPredictionApp/urls.py
+++ StockPredictionPortal/StockPredictionApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('c/index', views.configuration_index),
-    path('c/lst', views.configuration, name ="allConfiguration")
+    path('c/lst', views.configuration, name ="allConfiguration"),
+    path('c/edit', views.configuration, name ="edit"),
 ]
