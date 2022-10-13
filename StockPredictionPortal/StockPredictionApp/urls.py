4bb0bb041444f8f69d9e725799b0e6d8ca37262c
Pham Dinh Hau <phamdinhhau@gmail.com>
2022-10-13 22:06:41 +0700

    modified:   StockPredictionPortal/StockPredictionApp/templates/configuration.html 	modified:   StockPredictionPortal/StockPredictionApp/urls.py 	modified:   StockPredictionPortal/StockPredictionApp/views.py 	StockPredictionPortal/static/portal_site/plugins/jquery-cookie/

--- StockPredictionPortal/StockPredictionApp/urls.py
+++ StockPredictionPortal/StockPredictionApp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('c/index', views.configuration_index),
     path('c/lst', views.configuration, name ="allConfiguration"),
-    path('c/edit', views.configuration, name ="edit"),
+    path('c/add', views.configuration_add, name ="add"),
+    path('c/edit', views.configuration_edit, name ="edit"),
+    path('c/del', views.configuration_del, name ="del"),
 ]
