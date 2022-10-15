7149ddb13df0632c2fd0fea7d0e44eda206130aa
chauitvn <79033388+chauitvn@users.noreply.github.com>
2022-10-15 16:36:32 +0800

    update left menu

--- StockPredictionPortal/StockPredictionApp/urls.py
+++ StockPredictionPortal/StockPredictionApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('c/index', views.configuration_index),
+    path('c/index', views.configuration_index, name ="c_index"),
     path('c/lst', views.configuration, name ="allConfiguration"),
     path('c/add', views.configuration_add, name ="add"),
     path('c/edit', views.configuration_edit, name ="edit"),
